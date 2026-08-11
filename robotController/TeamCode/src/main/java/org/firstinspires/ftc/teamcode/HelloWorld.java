aaaab367e39035163df4a7f2d3d23669d3f93f33
sanskruthg <sanskruthg@gmail.com>
2026-08-10 18:29:55 -0700

    Update HelloWorld.java

--- robotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ robotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+// import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@Disabled
-
+// @Disabled
 @TeleOp
 public class HelloWorld extends OpMode {
 
