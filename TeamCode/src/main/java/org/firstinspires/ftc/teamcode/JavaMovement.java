776439a0c219ef3ffa00737a9f2b1b8bb498ed7f
sanskruthg <sanskruthg@gmail.com>
2026-08-15 11:37:35 -0700

    Update JavaMovement.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaMovement.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class JavaMovement extends OpMode {
+public class JavaMovement extends LinearOpMode {
 
     private DcMotor frontLeftMotor = null;
     private DcMotor frontRightMotor = null;
@@ -14,22 +15,58 @@ public class JavaMovement extends OpMode {
 
 
     @Override
-    public void init() {
+    public void runOpMode() {
         frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
-        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                telemetry.update();
+                if (gamepad1.left_stick_y < 0) {
+                    frontRightMotor.setPower(0.5);
+                    frontLeftMotor.setPower(-0.5);
+                    backRightMotor.setPower(0.5);
+                    backLeftMotor.setPower(-0.5);
+                }
+                if (gamepad1.left_stick_y > 0) {
+                    backRightMotor.setPower(-0.5);
+                    backLeftMotor.setPower(0.5);
+                    frontLeftMotor.setPower(0.5);
+                    frontRightMotor.setPower(-0.5);
+                }
+                if (gamepad1.left_stick_x < 0) {
+                    frontLeftMotor.setPower(-0.5);
+                    frontRightMotor.setPower(0.5);
+                    backLeftMotor.setPower(0.5);
+                    backRightMotor.setPower(-0.5);
+                }
+                if (gamepad1.left_stick_x > 0) {
+                    backRightMotor.setPower(0.5);
+                    backLeftMotor.setPower(-0.5);
+                    frontRightMotor.setPower(-0.5);
+                    frontLeftMotor.setPower(0.5);
+                }
+                if (gamepad1.left_stick_x == 0) {
+                    frontRightMotor.setPower(0);
+                    frontLeftMotor.setPower(0);
+                    backRightMotor.setPower(0);
+                    backLeftMotor.setPower(0);
+                }
+                if (gamepad1.left_stick_y == 0) {
+                    backRightMotor.setPower(0);
+                    backLeftMotor.setPower(0);
+                    frontRightMotor.setPower(0);
+                    frontLeftMotor.setPower(0);
+                }
+            }
+        }
+
+
     }
 
-    @Override
-    public void loop() {
-
-    }
 }
