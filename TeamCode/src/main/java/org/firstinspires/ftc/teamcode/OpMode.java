91a8d687a81801793d09d2733cedf59c29966f34
sanskruthg <100040977+sanskruthg@users.noreply.github.com>
2026-09-01 17:29:20 -0700

    9/1/26
    Co-Authored-By: RoboMavericks <320395851+robomavericks@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -63,8 +62,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-public class Op extends LinearOpMode {
+@TeleOp //(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+public class OpMode extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
