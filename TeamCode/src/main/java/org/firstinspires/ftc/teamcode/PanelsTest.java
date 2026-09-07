b4333ba3dc5f8e8b4f9cb9685fe4af8d8e1e58b2
sanskruthg <100040977+sanskruthg@users.noreply.github.com>
2026-09-07 16:40:54 -0700

    Added Bezier Curve
    9.7.26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PanelsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PanelsTest.java
@@ -1,9 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 
-
 import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
@@ -14,11 +12,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 //import org.firstinspires.ftc.teamcode.drawing;
 //import org.firstinspires.ftc.teamcode.pedroPathing.Tuning;
-//import com.pedropathing.ivy.Command;
-//import com.pedropathing.ivy.behaviors.*;
 
 @Autonomous
-public class AutoTest extends OpMode {
+public class PanelsTest extends OpMode {
 
     private Follower follower;
     private ElapsedTime timer;
@@ -34,7 +30,6 @@ public class AutoTest extends OpMode {
 
     private final Pose startPose = new Pose(72,72, Math.toRadians(0));
     private final Pose endPose = new Pose(30,30, Math.PI);
-    private final Pose curvePose = new Pose(120,30);
 
     private PathChain pathChain1;
     private PathChain pathChain2;
@@ -51,7 +46,7 @@ public class AutoTest extends OpMode {
         pathChain1 = follower.pathBuilder()
                 .addPath(new BezierLine(startPose, endPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading())
-                .addPath(new BezierCurve(endPose, curvePose, startPose))
+                .addPath(new BezierLine(endPose, startPose))
                 .setLinearHeadingInterpolation(endPose.getHeading(), startPose.getHeading())
                 .build();
 
@@ -72,7 +67,6 @@ public class AutoTest extends OpMode {
         follower.update();
         drawing.drawDebug(follower);
 
-        //Need to replace with IVY
         /*
         switch (currentState) {
             case PATH_1:
